95798003f99daa02b4479c6f7bf9a75b82a912de
Subhodeep Moitra <smoitra@cs.cmu.edu>
2012-04-23 15:51:34 -0400

    seq_analyzer.py

--- hivtime/seq_analyzer.py
+++ hivtime/seq_analyzer.py
@@ -5,6 +5,7 @@
 
 
 import sys,os,pdb
+from operator import itemgetter
 
 import numpy as np
 import pylab as pl
@@ -13,22 +14,74 @@ import Bio
 
 import utility as util
 
+#----------------------------------------------------------------------
+# Constants and definitions
+dbpath = 'hivtime.db' 
+datadir = util.get_datadir()
 
+dbname = 'hivtime.db'
+dbpath = os.path.join(util.get_datadir(),dbname)
+#tbls = ['p24.tbl','full_genome.tbl']
+tbls = ['full_genome.tbl']
+tbls = [os.path.join(util.get_datadir(),tbl) for tbl in tbls]
+
+
+
+#----------------------------------------------------------------------
+# Scripts
+
+def get_tblname_from_fpath(fpath) : 
+	""" Get the names of the tables """
+	fname = os.path.split(fpath)[1]
+	tblname = os.path.splitext(fname)[0]
+	return tblname
 
 class SeqAnalyzer(object)  :
 	""" Performs analysis of sequences"""
-	def __init__(self) : 
-		self.db_name = ""
+	def __init__(self,cur,con,tblname) : 
+		self.cur = cur
+		self.con = con
+		self.name = tblname
+		self.header = map(itemgetter(1),self._get_header_meta())
+		self.pcodes = self._get_patientcodes()
 
 	def analyze(self) : 
 		""" Analyze all the sequences """
-		pass
-
-	def find_pids(self) : 
-		""" Read the db"""
-		pass	
+		print("Currently Analyzing %s"%(self.name))
 
 
-if __name__ == '__main__' : 
-	seq_analyzer =  SeqAnalyzer();
-	seq_analyzer.analyze()
+	def _get_header_meta(self) : 
+		""" Get the header metadata """	
+		cur.execute("PRAGMA table_info(%s)"%self.name)
+		return cur.fetchall()
+
+	def _get_patientcodes(self)  :
+		""" Get all the patientcodes"""
+		cur.execute("SELECT DISTINCT patientcode from %s\
+			WHERE patientcode is NOT NULL"%(self.name))
+		return map(itemgetter(0),cur.fetchall())
+		
+
+	def find_time_pids(self) : 
+		"""
+Read the db to get patient ids of patients that have reasonable amount
+of time series information
+		"""
+		cmd_str = "SELECT * from "
+
+#-----------------------------------------------------------------------
+# Main Script
+#-----------------------------------------------------------------------
+
+with lite.connect(dbpath) as con : 
+	# Check SQL version for sanity check
+	cur = con.cursor()
+	cur.execute("SELECT SQLITE_VERSION()")
+	print "SQL Version %s"%cur.fetchone()
+
+	# Extract tablenames from full path	
+	tblnames = map(get_tblname_from_fpath,tbls)
+		
+	for tblname in tblnames : 
+		seq_analyzer =  SeqAnalyzer(cur,con,tblname);
+		seq_analyzer.analyze()
