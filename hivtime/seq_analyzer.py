1a9faa667dbb3922dd6e8c78ec1e233ca3146db3
Subhodeep Moitra <smoitra@cs.cmu.edu>
2012-04-24 14:18:34 -0400

    Extract patient codes who have enough time data

--- hivtime/seq_analyzer.py
+++ hivtime/seq_analyzer.py
@@ -22,7 +22,7 @@ datadir = util.get_datadir()
 dbname = 'hivtime.db'
 dbpath = os.path.join(util.get_datadir(),dbname)
 #tbls = ['p24.tbl','full_genome.tbl']
-tbls = ['full_genome.tbl']
+tbls = ['p24.tbl']
 tbls = [os.path.join(util.get_datadir(),tbl) for tbl in tbls]
 
 
@@ -44,15 +44,17 @@ class SeqAnalyzer(object)  :
 		self.name = tblname
 		self.header = map(itemgetter(1),self._get_header_meta())
 		self.pcodes = self._get_patientcodes()
+		self.time_fields = ('SamplingYear','DaysfromfirstSample',\
+			'FiebigStage','DaysfromInfection','DaysfromSeroconversion')
+		self.pcodes_time = None
 
 	def analyze(self) : 
 		""" Analyze all the sequences """
 		print("Currently Analyzing %s"%(self.name))
 		
 		# Find patients who have enough time data
-		self.find_time_pids() 
-
-
+		self.pcodes_time = self._find_time_pids() 
+		
 	def _get_header_meta(self) : 
 		""" Get the header metadata """	
 		cur.execute("PRAGMA table_info(%s)"%self.name)
@@ -65,34 +67,100 @@ class SeqAnalyzer(object)  :
 		return map(itemgetter(0),cur.fetchall())
 		
 
-	def find_time_pids(self) : 
+	def _find_time_pids(self) : 
 		"""
 Read the db to get patient ids of patients that have reasonable amount
 of time series information
 
 # Criteria to use ??
-Patients who have atleast 4 time points any field of the 5 discriminating fields
+Patients who have atleast 4 time points any field of the 5
+discriminating fields
+
+What to do if multiple time fields have discriminating information ?
+Pick timeforsero > timeinfec > dayssample > fiebig > sampling year
 		"""
 		cur = self.cur
-		n_filter = 2
-		for iter_filter in range(n_filter,n_filter+15) : 
-			cmd_str = "DROP TABLE IF EXISTS dummy1"
-			cur.execute(cmd_str)
-			cmd_str = "CREATE TABLE dummy1 AS SELECT patientcode,A,B,C,D,E \
-	FROM (SELECT \
-	 patientcode,COUNT(DISTINCT daysFROMfirstsample) AS A, \
-	COUNT(DISTINCT daysFROMinfection) AS B, COUNT(DISTINCT \
-	daysFROMseroconversion) AS C,COUNT(DISTINCT samplingyear) AS D,\
-	COUNT(DISTINCT fiebigstage) AS E FROM p24 GROUP BY patientcode) \
-	where A>=%d OR B>=%d OR C>=%d OR D>=%d OR E>=%d ;"%\
-				((iter_filter,)*5)
-			cur.execute(cmd_str)
-			cmd_str = "SELECT count(patientcode) from dummy1"
-			cur.execute(cmd_str)
-			count_pats = cur.fetchone()[0]
-			print("The number of patients satisfying %d criteria=%d"%\
-					(iter_filter,count_pats))
+		# REMOVEME : Going to work with n_filter = 11 items first for 
+		# debug stage
+		n_filter = 11
+		cmd_str = "DROP TABLE IF EXISTS dummy1"
+		cur.execute(cmd_str)
+		field_codes = 'ABCDE'
+		field_code_dict = dict(zip(field_codes,self.time_fields))
+		field_code_dict2 = dict(zip(self.time_fields,field_codes))
+
+		cmd_str1 = ",".join(field_codes)
+		cmd_str2 = ", ".join(["COUNT(DISTINCT %s) AS %s"%(f[0],f[1])\
+			for f in zip(self.time_fields,field_codes)])
+		cmd_str3 = " OR ".join(["%s>="%(f)+"%d" for f in field_codes])	
+		cmd_str3 = cmd_str3%((n_filter,)*len(field_codes))
+		cmd_str = "CREATE TABLE dummy1 AS SELECT patientcode," + \
+			cmd_str1 + " FROM (SELECT patientcode," + cmd_str2 + \
+			" FROM %s GROUP BY patientcode) WHERE "%(self.name) + \
+			cmd_str3 + " ;" 
+
+		# Example command from above construct
+#		cmd_str = "CREATE TABLE dummy1 AS SELECT patientcode,A,B,C,D,E \
+#FROM (SELECT \
+# patientcode,COUNT(DISTINCT daysFROMfirstsample) AS A, \
+#COUNT(DISTINCT daysFROMinfection) AS B, COUNT(DISTINCT \
+#daysFROMseroconversion) AS C,COUNT(DISTINCT samplingyear) AS D,\
+#COUNT(DISTINCT fiebigstage) AS E FROM p24 GROUP BY patientcode) \
+#where A>=%d OR B>=%d OR C>=%d OR D>=%d OR E>=%d ;"%\
+#			((n_filter,)*5)
 		
+		cur.execute(cmd_str)
+		cmd_str = "SELECT count(patientcode) from dummy1"
+		cur.execute(cmd_str)
+		count_pats = cur.fetchone()[0]
+		print("The number of patients satisfying criteria %d = %d"%\
+				(n_filter,count_pats))
+			
+		# Find the fields which have the excess time points as well
+		# Patient codes satisfying the filter constraints
+
+		# Dicts of patientcodes and their correpsonding fields
+		# This is returned from func 
+		pcodes_time = {}
+
+		#Priority of time fields
+		priority_time_fields = ('DaysfromSeroconversion',\
+			'DaysfromInfection','DaysfromfirstSample','SamplingYear',\
+			'FiebigStage')
+
+		priority_field_codes = [field_code_dict2[f] for f in \
+			priority_time_fields]
+	
+		# Loop through all the fields that have the information
+		for field in priority_field_codes : 
+			cmd_str = "SELECT patientcode,%s FROM dummy1  where %s \
+				>= %d"%(field,field,n_filter)
+			cur.execute(cmd_str)
+			records = cur.fetchall()
+			pcodes = map(itemgetter(0),records)
+			for pcode in pcodes :
+				if not pcodes_time.has_key(pcode) : 
+					pcodes_time[pcode] = field_code_dict[field]
+
+		return pcodes_time
+
+#		#DEPRECEATED CODE. REMOVE in future commits
+#		# Retreive all records
+#		cmd_str = "SELECT * FROM dummy1"
+#		cur.execute(cmd_str)
+#		records = cur.fetchall()
+#		filtered_recs = {}
+#		
+#		filt_fn1 = lambda x,field_idx : x[field_idx] >= n_filter
+#		
+#		# Loop through all the fields to find filter criteria
+#		for idx_field in range(1,len(field_codes)+1) : 
+#			filt_fn2 = lambda x : filt_fn1(x,idx_field)
+#			filt_records = filter(filt_fn2,records)
+#			filtered_recs[field_codes[idx_field-1]] = filt_records
+#
+#		# Return patients and the field which has the time info
+					
 
 
 #-----------------------------------------------------------------------
